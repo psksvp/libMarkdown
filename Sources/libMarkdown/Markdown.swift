09622b7d91800144701bd75c47f5449bbd54f1af
psksvp <psksvp>
2022-08-30 18:39:32 +1000

    markdown filter to have caption for Image, Mermaid, and  Table

--- Sources/libMarkdown/Markdown.swift
+++ Sources/libMarkdown/Markdown.swift
@@ -145,9 +145,12 @@ public class Markdown
     public func run(_ block: String) -> String?
     {
       return """
+      <figure>
       <div class="mermaid">
       \(block)
       </div>
+      <figcaption>Fig.1 - Trulli, Puglia, Italy.</figcaption>
+      </figure>
       """
     }
   }
@@ -173,134 +176,17 @@ public class Markdown
       let reader = CSVReader(with: block)
 
       return """
+             <figure>
              <table>
              \(row(reader.headers))
              \(reader.rows.map{row($0)}.joined(separator: "\n"))
              </table>
+             <figcaption>Fig.1 - Trulli, Puglia, Italy.</figcaption>
+             </figure>
              """
     }
     
-//    public func run(_ block: String) -> String?
-//    {
-//      let reader = CSVReader(with: block)
-//
-//      let result = """
-//      \(reader.headers.joined(separator: "|"))
-//      \(String(repeating: "---|", count: reader.headers.count))
-//      \(reader.rows.map{$0.joined(separator: "|")}.joined(separator: "\n"))
-//      """
-//      return result
-//    }
   }
   
 }
 
-
-
-/////////////////////
-//class func runfilters(_ md: String) -> String
-//{
-//  var mdf = filterAsciiMath(md)
-//  if mdf.contains("csvtable")
-//  {
-//    mdf = Markdown.filterCSV(mdf)
-//  }
-//
-//  if mdf.contains("mermaid")
-//  {
-//    mdf = Markdown.filterMermaid(mdf)
-//  }
-//
-//  return mdf
-//}
-//  //// REFACTOR HERE
-//  class func csv2MdTable(_ csv: String) -> String?
-//  {
-//    let reader = CSVReader(with: csv)
-//
-//    let result = """
-//    \(reader.headers.joined(separator: "|"))
-//    \(String(repeating: "---|", count: reader.headers.count))
-//    \(reader.rows.map{$0.joined(separator: "|")}.joined(separator: "\n"))
-//    """
-//    return result
-//  }
-//
-//  class func filterCSV(_ md: String) -> String
-//  {
-//    var result = md
-//
-//    let pattern = #"(?s)~~~\s*csvtable\s*(.*?)~~~"#
-//    let regex = try? NSRegularExpression(pattern: pattern, options:[])
-//    for (_, csvBlock) in md.liftRegexPattern(pattern)
-//    {
-//      if let match = regex?.firstMatch(in: csvBlock,
-//                                       options: [],
-//                                       range: NSRange(csvBlock.startIndex..<csvBlock.endIndex, in: csvBlock)),
-//         let range = Range(match.range(at: 1), in: csvBlock),
-//         let table = csv2MdTable(String(csvBlock[range]))
-//      {
-//        result = result.replacingOccurrences(of: csvBlock, with: table)
-//      }
-//    }
-//
-//    return result
-//  }
-//
-//
-//  class func filterMermaid(_ md: String) -> String
-//  {
-//    func toMermaidDiv(_ s: String) -> String
-//    {
-//      return """
-//      <div class="mermaid">
-//      \(s)
-//      </div>
-//      """
-//    }
-//
-//    var result = md
-//
-//    let pattern = #"(?s)~~~\s*mermaid\s*(.*?)~~~"#
-//    let regex = try? NSRegularExpression(pattern: pattern, options:[])
-//    for (_, mmBlock) in md.liftRegexPattern(pattern)
-//    {
-//      if let match = regex?.firstMatch(in: mmBlock,
-//                                       options: [],
-//                                       range: NSRange(mmBlock.startIndex..<mmBlock.endIndex, in: mmBlock)),
-//         let range = Range(match.range(at: 1), in: mmBlock)
-//      {
-//        let mermaidDiv = toMermaidDiv(String(mmBlock[range]))
-//        result = result.replacingOccurrences(of: mmBlock, with: mermaidDiv)
-//      }
-//    }
-//
-//    return result
-//  }
-//
-//
-//  /*
-//
-//   <`(.*?)`>
-//   */
-//
-//  class func filterAsciiMath(_ md: String) -> String
-//  {
-//    var result = md
-//
-//    let pattern = #"<`(.*?)`>"#
-//    let regex = try? NSRegularExpression(pattern: pattern, options:[])
-//    for (_, mmBlock) in md.liftRegexPattern(pattern)
-//    {
-//      if let match = regex?.firstMatch(in: mmBlock,
-//                                       options: [],
-//                                       range: NSRange(mmBlock.startIndex..<mmBlock.endIndex, in: mmBlock)),
-//         let range = Range(match.range(at: 1), in: mmBlock)
-//      {
-//        let backTick = "`` `\(String(mmBlock[range]).trim())` ``"
-//        result = result.replacingOccurrences(of: mmBlock, with: backTick)
-//      }
-//    }
-//
-//    return result
-//  }
